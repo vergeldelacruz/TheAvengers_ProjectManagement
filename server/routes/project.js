b25c024cc88a7514fef3ae61694ac1ab4b49463e
Vergel dela Cruz <vergel@gmail.com>
2022-11-18 13:17:05 -0500

    second commit

--- server/routes/project.js
+++ server/routes/project.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 const router = express.Router();
 const auth = require("../middleware/auth");
 
