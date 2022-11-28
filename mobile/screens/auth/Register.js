545291863ca8ae2f6a6fe8d9c0716383a5860472
Alfy524 <alfy524@gmail.com>
2022-11-28 17:54:28 -0500

    registration completed

--- mobile/screens/auth/Register.js
+++ mobile/screens/auth/Register.js
@@ -8,96 +8,167 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUser } from "../../store/auth/authActions";
+import { commonStyles, formStyles } from "../../theme/styles";
 
 const Register = ({ navigation }) => {
 
+  const { theme } = useSelector((state) => state.commonReducer);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [jobTitle, setJobTitle] = useState("");
   const [role, setRole] = useState("");
+  const { error } = useSelector((state) => state.authReducer);
+
+  const dispatch = useDispatch();
 
   const onSignUp = () => {
-
+    dispatch(
+      registerUser({
+        email,
+        password,
+        firstName,
+        lastName,
+        jobTitle,
+        role,
+      })
+    );
   };
+  useEffect(() => {
+    if (error) Alert.alert(error);
+  }, [error]);
+
   const onExistingUser = () => {
     navigation.navigate("Login");
   };
   return (
-    <ScrollView>
-      <View style={styles.container}>
+    <View
+      style={{
+        ...commonStyles.mainContainer,
+        flex: 1,
+        backgroundColor: theme.background,
+      }}
+    >
+      <ScrollView>
         <SafeAreaView>
+          <Text style={{ ...commonStyles.mainHeading, color: theme.dark }}>
+            Register to Continue
+          </Text>
 
-          <Text style={styles.title}>Sign up</Text>
+
           <View style={styles.inputWrapper}>
-            <Text style={styles.label}>First Name</Text>
+            <Text style={{ ...formStyles.label, color: theme.darkGrey }}>
+              First Name
+            </Text>
             <TextInput
-              style={styles.input}
               placeholder="Your First Name"
+              placeholderTextColor={theme.darkGrey}
+              style={{
+                ...formStyles.input,
+                backgroundColor: theme.light,
+                color: theme.dark,
+              }}
               value={firstName}
               onChangeText={(a) => setFirstName(a)}
             />
           </View>
           <View style={styles.inputWrapper}>
-            <Text style={styles.label}>Last Name</Text>
+            <Text style={{ ...formStyles.label, color: theme.darkGrey }}>
+              Last Name
+            </Text>
             <TextInput
-              style={styles.input}
               placeholder="Your Last Name"
+              placeholderTextColor={theme.darkGrey}
+              style={{
+                ...formStyles.input,
+                backgroundColor: theme.light,
+                color: theme.dark,
+              }}
               value={lastName}
               onChangeText={(a) => setLastName(a)}
             />
           </View>
           <View style={styles.inputWrapper}>
-            <Text style={styles.label}>Email</Text>
+            <Text style={{ ...formStyles.label, color: theme.darkGrey }}>
+              Email
+            </Text>
             <TextInput
-              style={styles.input}
-              placeholder="Your Email"
+              style={{
+                ...formStyles.input,
+                backgroundColor: theme.light,
+                color: theme.dark,
+              }}
+              placeholderTextColor={theme.darkGrey}
+              placeholder="eg. abcd@gmail.com"
               value={email}
               onChangeText={(a) => setEmail(a)}
             />
           </View>
           <View style={styles.inputWrapper}>
-            <Text style={styles.label}>Password</Text>
+            <Text style={{ ...formStyles.label, color: theme.darkGrey }}>
+              Password
+            </Text>
             <TextInput
               placeholder="Your Password"
-              style={styles.input}
+              placeholderTextColor={theme.darkGrey}
+              style={{
+                ...formStyles.input,
+                backgroundColor: theme.light,
+                color: theme.dark,
+              }}
               value={password}
               onChangeText={(a) => setPassword(a)}
             />
           </View>
           <View style={styles.inputWrapper}>
-            <Text style={styles.label}>Job Title</Text>
+            <Text style={{ ...formStyles.label, color: theme.darkGrey }}>
+              Job Title
+            </Text>
             <TextInput
-              placeholder="Your Job"
-              style={styles.input}
+              placeholder="Your Job Title"
+              placeholderTextColor={theme.darkGrey}
+              style={{
+                ...formStyles.input,
+                backgroundColor: theme.light,
+                color: theme.dark,
+              }}
               value={jobTitle}
               onChangeText={(a) => setJobTitle(a)}
             />
           </View>
           <View style={styles.inputWrapper}>
-            <Text style={styles.label}>Role</Text>
+            <Text style={{ ...formStyles.label, color: theme.darkGrey }}>
+              Role
+            </Text>
             <TextInput
               placeholder="Your Role"
-              style={styles.input}
+              placeholderTextColor={theme.darkGrey}
+              style={{
+                ...formStyles.input,
+                backgroundColor: theme.light,
+                color: theme.dark,
+              }}
               value={role}
               onChangeText={(a) => setRole(a)}
             />
           </View>
-          <TouchableOpacity style={styles.button} onPress={onSignUp}>
-            <Text style={styles.buttonLabel}>Register</Text>
+          <TouchableOpacity style={formStyles.submitButton} onPress={onSignUp}>
+            <Text style={formStyles.buttonText}>Register</Text>
           </TouchableOpacity>
           <View style={styles.linksWrapper}>
             <TouchableOpacity onPress={onExistingUser}>
-              <Text>Existing User?</Text>
+              <Text style={{ ...formStyles.label, color: theme.darkGrey }}>Existing User?</Text>
             </TouchableOpacity>
             <TouchableOpacity>
-              <Text>Forgot Password?</Text>
+              <Text style={{ ...formStyles.label, color: theme.darkGrey }}>Forgot Password?</Text>
             </TouchableOpacity>
           </View>
         </SafeAreaView>
-      </View>
-    </ScrollView>
+      </ScrollView>
+    </View>
   );
 }
 export default Register
@@ -110,36 +181,8 @@ const styles = StyleSheet.create({
     marginTop: 40,
     display: "flex",
   },
-  title: {
-    fontSize: 20,
-    fontWeight: "500",
-    marginBottom: 20,
-  },
   inputWrapper: {
     marginVertical: 10,
-  },
-  label: {
-    fontSize: 16,
-    marginBottom: 10,
-  },
-  input: {
-    height: 60,
-    padding: 10,
-    backgroundColor: "#fff",
-  },
-  button: {
-    marginVertical: 10,
-    width: "100%",
-    height: 60,
-    backgroundColor: "#000",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  buttonLabel: {
-    color: "white",
-    fontWeight: "400",
-    fontSize: 20,
   },
   linksWrapper: {
     display: "flex",
